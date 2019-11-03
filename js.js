c07e19241dcc947586f9080017d214222b6739a1
Phạm Thu Huyền <ThuHuyen@MacBook-Air-cua-Pham.local>
2019-11-03 09:51:22 +0700

    final

--- js.js
+++ js.js
@@ -1,6 +1,46 @@
 show_number = 1
 document.addEventListener('keypress', (e) => {
-    document.getElementById('div'+show_number).style.display = "block"
+    document.getElementById('div'+show_number).style.display = "inline-block"
     show_number +=1
 })
-
+show_number = 1
+document.addEventListener('keypress', (e) => {
+    document.getElementById('diav'+show_number).style.display = "inline-block"
+    show_number +=1
+})
+function chonNoidung1(){
+    window.location.href = "./lost2.html"
+}
+function chonNoidung2(){
+    window.location.href = "./turnleft.html"
+}
+function chonNoidung3(){
+    window.location.href = "./lost3.html"
+}
+function chonNoidung4(){
+    window.location.href = "./lost5.html"
+}
+function chonNoidung5(){
+    window.location.href = "./lost4.html"
+}
+function chonNoidung6(){
+    window.location.href = "./lost6.html"
+}
+function chonNoidung7(){
+    window.location.href = "./lost7.html"
+}
+function chonNoidung11(){
+    window.location.href = "./turnleft1.html"
+}
+function chonNoidung12(){
+    window.location.href = "./ending3.html"
+}
+function chonNoidung13(){
+    window.location.href = "./lost7.html"
+}
+function chonNoidung20(){
+    window.location.href = "./lost8.html"
+}
+function chonNoidung10(){
+    window.location.href = "./endlast.html"
+}
